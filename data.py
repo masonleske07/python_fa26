d0408d06eca7048399820b1d1579e4eb69317c09
Mason Leske <masonleske717@yahoo.com>
2026-09-03 13:40:51 -0500

    Add files via upload

---
+++ data.py
@@ -0,0 +1,30 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS (DO NOT DELETE)
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. Define a String variable.
+[ ] 3. Define an Integer variable.
+[ ] 4. Define a Float variable.
+[ ] 5. Define a Boolean variable.
+[ ] 6. Print all variables using F-Strings.
+[ ] 7. Upload to GitHub.
+-----------------------------------------------------------------------
+"""
+# ℹ️ Assignment name: Data Types
+# ℹ️ Date: August 27, 2026
+# ℹ️ File Name: data.py
+
+# ℹ️ Numbers
+
+name = "Mason"
+age = 19
+height_in_feet = 5.92
+single = True
+
+# Print out Information
+print(f"Name: {name}")
+print(f"Age: {age} years old")
+print(f"Height: {height_in_feet} ft")
+print(f"Is Single? {single}")
+
